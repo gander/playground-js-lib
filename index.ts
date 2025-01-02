84b18563c0bebc6b455c2e49f9ad1eb9ca41405b
Adam Gąsowski <gander@users.noreply.github.com>
2025-01-02 21:10:18 +0100

    feat @vue/reactivity

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
+import {computed, Ref} from '@vue/reactivity';
+
 export function add(a: number, b: number): number {
     return a + b;
+}
+
+export function addRef(a: Ref<number>, b: Ref<number>): Ref<number> {
+    return computed(() => a.value + b.value)
 }
 
 console.log('Add 2 + 3 =', add(2, 3));
