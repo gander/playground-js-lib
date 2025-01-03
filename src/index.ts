32c236c2f5348676b1f7722f837c8b6c0263d46d
Adam Gąsowski <gander@users.noreply.github.com>
2025-01-03 06:39:33 +0100

    add docs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import {computed, type Ref} from '@vue/reactivity';
 
+/** This function adds the two passed numbers together. */
 export function add(a: number, b: number): number {
     return a + b;
 }
 
+/**
+ * This function adds the two passed references with numbers together.
+ * The return type is also new reference with number
+ *
+ * @example
+ * ```ts
+ * import { ref } from "@vue/reactivity"
+ * import { addRef } from "@gander/playground"
+ *
+ * const partA = ref(123);
+ * const partB = ref(321);
+ *
+ * const result = addRef(partA, partB);
+ *
+ * console.log(result.value);
+ * ```
+ */
 export function addRef(a: Ref<number>, b: Ref<number>): Ref<number> {
     return computed(() => a.value + b.value)
 }
 
+/** Outputs example of sum */
 console.log('Add 2 + 3 =', add(2, 3));
 
